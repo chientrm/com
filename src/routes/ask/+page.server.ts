db23a1775ea47bc62e91ccc232506cd211b9d1f6
chientrm <chientrm@gmail.com>
2023-07-06 02:33:26 +0000

    temporarily remove page limit

--- src/routes/ask/+page.server.ts
+++ src/routes/ask/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from '../$types';
 
 export const load = (async ({ locals }) => {
   const result = await locals.D1.prepare(
-      'select id, username, content, createdAt from Com_Ask where parentId is null order by createdAt DESC limit 10'
+      'select id, username, content, createdAt from Com_Ask where parentId is null order by createdAt DESC'
     ).all<{ id: number; username: string; content: string; createdAt: Date }>(),
     asks = (result.results ?? []).map((ask) => ({
       ...ask,
