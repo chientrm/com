fe9720a00206f79b8ac2180dfe84df5074d8f447
chientrm <chientrm@gmail.com>
2023-07-06 02:38:43 +0000

    back button

--- src/routes/ask/[id]/+page.svelte
+++ src/routes/ask/[id]/+page.svelte
@@ -7,41 +7,51 @@
   export let form: ActionData;
 </script>
 
-<Ask
-  index={0}
-  id={data.id}
-  content={data.content}
-  username={data.username}
-  fromNow={data.fromNow}
-/>
+<div class="col">
+  {#if data.parentId}
+    <a href={`/ask/${data.parentId}`}>← back</a>
+  {/if}
 
-<hr />
+  <Ask
+    index={0}
+    id={data.id}
+    content={data.content}
+    username={data.username}
+    fromNow={data.fromNow}
+  />
 
-<div class="replies">
-  {#each data.replies as { id, content, username, fromNow }, index}
-    <Ask {index} {id} {content} {username} {fromNow} />
-  {/each}
+  <hr />
+
+  <div class="replies">
+    {#each data.replies as { id, content, username, fromNow }, index}
+      <Ask {index} {id} {content} {username} {fromNow} />
+    {/each}
+  </div>
+
+  <form method="POST" action="?/reply" use:enhance>
+    <Error message={form?.message} />
+    <table>
+      <tr>
+        <td>reply:</td>
+        <td>
+          <textarea name="reply" value={data.reply} maxlength="5000" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <button>submit</button>
+        </td>
+        <td />
+      </tr>
+    </table>
+  </form>
 </div>
 
-<form method="POST" action="?/reply" use:enhance>
-  <Error message={form?.message} />
-  <table>
-    <tr>
-      <td>reply:</td>
-      <td>
-        <textarea name="reply" value={data.reply} maxlength="5000" />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <button>submit</button>
-      </td>
-      <td />
-    </tr>
-  </table>
-</form>
-
 <style>
+  div.col {
+    display: flex;
+    flex-direction: column;
+  }
   div.replies {
     padding: 8pt;
     padding-left: 16pt;
