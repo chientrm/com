a37d284ec92c279ad108a9dc9d0f68fdc7b1d17f
chientrm <chientrm@gmail.com>
2023-07-18 13:32:48 +0000

    fix total count

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,18 +3,18 @@ import type { LayoutServerLoad } from './$types';
 export const load = (async ({ locals, request }) => {
   const { user, colorMode } = locals,
     url = new URL(request.url),
-    pathname = url.pathname;
+    pathname = url.pathname,
+    { count: totalCount } = await locals.D1.prepare(
+      'select count(*) as count from Com_Ask where parentId is null'
+    ).first<{ count: number }>();
   if (user) {
     const { username } = user,
       { count } = await locals.D1.prepare(
         'select count(id) as count from Com_Ask where username != ?1 and parentId in (select id from Com_Ask where username=?1)'
       )
         .bind(username)
-        .first<{ count: number }>(),
-      { count: totalCount } = await locals.D1.prepare(
-        'select count(*) as count from Com_Ask where parentId is null'
-      ).first<{ count: number }>();
+        .first<{ count: number }>();
     return { user, colorMode, pathname, count, totalCount };
   }
-  return { user, colorMode, pathname };
+  return { user, colorMode, pathname, totalCount };
 }) satisfies LayoutServerLoad;
