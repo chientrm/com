54747724ca8529de138adf4a2737e80b3da64913
chientrm <chientrm@gmail.com>
2023-07-18 13:08:43 +0000

    add some og

--- src/routes/are-you-not-entertained/+layout.svelte
+++ src/routes/are-you-not-entertained/+layout.svelte
@@ -1,9 +1,18 @@
+<script>
+  const title = 'Are you not entertained?',
+    description = 'Highly entertainted tweets';
+</script>
+
 <svelte:head>
   <script
     async
     src="https://platform.twitter.com/widgets.js"
     charset="utf-8"
   ></script>
+  <title>chientrm.com | {title}</title>
+  <meta name="description" content={description} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
 </svelte:head>
 
 <slot />
