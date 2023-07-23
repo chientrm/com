3ef56af2b316fe182f310e5b4f9d2466c85770c5
chientrm <chientrm@gmail.com>
2023-07-23 21:40:27 +0000

    coords done right

--- src/lib/helpers/coords.test.ts
+++ src/lib/helpers/coords.test.ts
@@ -3,14 +3,12 @@ import { cartesian2Polar, polar2Cartesian } from './coords';
 
 describe('coords', () => {
   it('coords', () => {
-    const lat = 124.45053144586961,
-      lng = -9.1801897109863262,
+    const lng = 124.45053144586961,
+      lat = -9.1801897109863262,
       r = 150,
       [x, y, z] = polar2Cartesian(lat, lng, r),
-      [newLat, newLng] = cartesian2Polar(x, y, z);
-    console.log({ lat, lng });
-    console.log({ newLat, newLng });
-    expect(newLat).toBe(lat);
-    expect(newLng).toBe(lng);
+      [newLng, newLat] = cartesian2Polar(x, y, z);
+    expect(newLat.toFixed(5)).equal(lat.toFixed(5));
+    expect(newLng.toFixed(5)).equal(lng.toFixed(5));
   });
 });
--- src/lib/helpers/coords.ts
+++ src/lib/helpers/coords.ts
@@ -2,7 +2,7 @@ const radian = 180 / Math.PI;
 
 const EPSILON = 1e-5;
 
-export function polar2Cartesian(lat: number, lng: number, r = 0) {
+export function polar2Cartesian(lng: number, lat: number, r = 0) {
   const phi = ((90.0 - lat) * Math.PI) / 180.0;
   const theta = ((90.0 - lng) * Math.PI) / 180.0;
   return [
@@ -18,8 +18,9 @@ export function cartesian2Polar(x: number, y: number, z: number) {
   const cylinderX = Math.sqrt(x * x + z * z),
     cylinderY = y,
     phi = Math.atan2(cylinderY, cylinderX),
-    theta = Math.atan2(z, x),
-    lat = 180.0 - (phi * 180.0) / Math.PI,
-    lng = -90.0 - (theta * 180.0) / Math.PI;
-  return [lat, lng];
+    theta = Math.atan2(x, z),
+    lng = 180.0 - (phi * 180.0) / Math.PI,
+    _lat = (theta * 180.0) / Math.PI,
+    lat = _lat > 90 ? _lat - 180 : _lat;
+  return [lng, lat];
 }
