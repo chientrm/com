746a86e4df35085bf1222c4d573dbbf5dccef8a2
chientrm <chientrm@gmail.com>
2023-07-23 08:03:03 +0000

    improve performance with page.ts globe

--- src/routes/globe/+page.ts
+++ src/routes/globe/+page.ts
@@ -1,8 +1,8 @@
+import type { PageLoad } from './$types';
 import groupsUrl from '$lib/assets/geojson/WB_countries_Admin0_lowres.geojson';
-import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
   const groups = await fetch(groupsUrl).then((res) => res.json<Group[]>()),
     radius = 150;
   return { radius, groups };
-}) satisfies PageServerLoad;
+}) satisfies PageLoad;
