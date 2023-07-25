8a23694fdd6199bbaed4b074c10df36c99630c08
chientrm <chientrm@gmail.com>
2023-07-25 01:15:56 +0000

    globe pick kinda working now

--- src/routes/globe/get_id/+server.ts
+++ src/routes/globe/get_id/+server.ts
@@ -1,4 +1,4 @@
-import countriesJson from '$lib/assets/geojson/WB_countries_Admin0_lowres.json';
+import countriesUrl from '$lib/assets/geojson/WB_countries_Admin0_lowres.json?url';
 import { cartesianToPolar } from '$lib/helpers/coords';
 import { validate } from '$lib/helpers/validate';
 import { json } from '@sveltejs/kit';
@@ -7,14 +7,16 @@ import type { FeatureCollection } from 'geojson';
 import { number } from 'yup';
 import type { RequestHandler } from './$types';
 
-export const POST = (async ({ request }) => {
+export const POST = (async ({ request, fetch }) => {
   const { x, y, z } = await validate(request, {
-    x: number().required(),
-    y: number().required(),
-    z: number().required()
-  });
-  const [lng, lat] = cartesianToPolar({ x, y, z }),
-    collection = countriesJson as FeatureCollection;
+      x: number().required(),
+      y: number().required(),
+      z: number().required()
+    }),
+    collection = await fetch(countriesUrl).then((res) =>
+      res.json<FeatureCollection>()
+    ),
+    [lng, lat] = cartesianToPolar({ x, y, z });
   for (const feature of collection.features) {
     if (
       booleanPointInPolygon(
