94e44991689cf891f4e6262335d393ecb731044a
chientrm <chientrm@gmail.com>
2024-02-02 15:03:56 +0000

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import { geograticule } from './plugins/geograticule';
 import { geojson } from './plugins/geojson';
 import { geoprop } from './plugins/geoprop';
