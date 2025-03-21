bbeae4b79de906ce46f7b5cc601ccc86b03a760f
chientrm <chientrm@gmail.com>
2025-03-21 17:19:21 +0700

    f

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+    return (
+        <div>
+            <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+                <img src={javascriptLogo} className="logo vanilla" alt="JavaScript logo" />
+            </a>
+            <h1>Hello Vite!</h1>
+            <div className="card">
+                <Counter />
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite logo to learn more
+            </p>
+        </div>
+    );
+}
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <button onClick={() => setCount((count) => count + 1)}>
+            Count is {count}
+        </button>
+    );
+}
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
