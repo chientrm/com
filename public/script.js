0bc2db587276666546334354fac196675be7d17b
chientrm <chientrm@gmail.com>
2025-03-21 14:53:12 +0700

    f

--- public/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const response = await fetch('/api/message');
-        const data = await response.json();
-        document.getElementById('message').textContent = data.message;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-});
