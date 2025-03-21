746483905e93e2b770c01d8174877b4ee2a4af79
chientrm <chientrm@gmail.com>
2025-03-21 17:29:27 +0700

    f

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { drizzle } from 'drizzle-orm/libsql';
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 const app = express();
 const PORT = 3000;
 
+const db = drizzle();
+
+export const usersTable = sqliteTable("users_table", {
+    username: text().primaryKey(),
+    passwordHash: text().notNull(),
+});
+
 // Serve static files from the "dist" directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(path.dirname(new URL(import.meta.url).pathname), 'dist')));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+    server: {
+        proxy: {
+            '/api': {
+                target: process.env.VITE_PROXY_TARGET,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+});
