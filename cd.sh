5977ca3eaa886d8d07eaf1637b4cb2d1da7a1a72
chientrm <chientrm@gmail.com>
2025-03-22 09:12:01 +0700

    f

---
+++ cd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull
+pnpm install
+pnpm run build
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: "com",
-      script: "server.js",
-      instances: 1, // Set to 'max' for cluster mode,
-      mode: "fork",
-      autorestart: true,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: 'com',
+            script: 'server.js',
+            instances: 1, // Set to 'max' for cluster mode,
+            mode: 'fork',
+            autorestart: true,
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
 };
