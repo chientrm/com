a37e7ea9ca317703f9cf8d570232b1061fa53d81
chientrm <chientrm@gmail.com>
2025-03-23 18:58:43 +0700

    f

--- server.js
+++ server.js
@@ -102,6 +102,12 @@ async function authenticateAdmin(req, res, next) {
     next();
 }
 
+// Middleware to log API calls
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 // Routes
 app.use(express.json());
 
