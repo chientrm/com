0b43d178ff939f782eecc88be41f92e647188f42
chientrm <chientrm@gmail.com>
2025-03-24 18:18:42 +0700

    f

--- server.js
+++ server.js
@@ -109,7 +109,18 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 const upload = multer({
-    dest: 'uploads/',
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/');
+        },
+        filename: (req, file, cb) => {
+            const uniqueSuffix = `${Date.now()}-${Math.round(
+                Math.random() * 1e9
+            )}`;
+            const originalName = file.originalname.replace(/\s+/g, '_');
+            cb(null, `${uniqueSuffix}-${originalName}`);
+        },
+    }),
     limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5 MB per file
     fileFilter: (req, file, cb) => {
         const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
